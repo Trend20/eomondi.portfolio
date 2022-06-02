b4f01f2d1aeee8d99c9a0da3eaa104f1bb4c2fe6
Enock Omondi <enockomondi305@2019>
2022-06-02 12:12:59 +0300

    updating project directory

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components';
+const Navbar = () =>{
+  return (
+    <header>
+      <div className="logo">
+        <img src="/img/logo.jpg" alt="logo" />
+      </div>
+      <nav>
+        <ul>
+          <li><a href="#"></a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
