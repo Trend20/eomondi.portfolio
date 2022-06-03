b0f787f85ed3f97508191a5da88e8be5efd3123d
Enock Omondi <enockomondi305@2019>
2022-06-03 10:11:15 +0300

    styling the about description

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,32 +7,32 @@ const AboutMe = () =>{
   return(
     <AboutWrapper className='about-me'>
         <Description className="description">
-          <h3>
-            <span>01.</span>
+          <H3>
+            <AboutSpan>01.</AboutSpan>
             About Me
-          </h3>
-          <div className="contents">
-          <p>
-            Hello! My name is Brittany and I enjoy creating things that live on the internet. My interest in web development started back in 2012 when I decided to try editing custom Tumblr themes — turns out hacking 
+          </H3>
+          <AboutMeContent className="contents">
+          <ContentParagraph>
+            Hello! My name is Enock and I enjoy creating things that live on the internet. My interest in web development started back in 2017 when I decided to try editing custom Tumblr themes — turns out hacking 
             together a custom reblog button taught me a lot about HTML & CSS!
-          </p>
-          <p>
+          </ContentParagraph>
+          <ContentParagraph>
             My main focus these days is building accessible, inclusive products and digital experiences for a variety of clients.
-          </p>
-          <p>
+          </ContentParagraph>
+          <ContentParagraph>
             Here are a few technologies I’ve been working with recently:
-          </p>
-          </div>
+          </ContentParagraph>
+          </AboutMeContent>
 
-          <div className="languages">
+          <TechStackContainer className="languages">
             {
               technologies.map((tech, index) =>{
                 return(
-                  <li key={index}>{tech}</li>
+                  <List key={index}>{tech}</List>
                 )
               })
             }
-          </div>
+          </TechStackContainer>
         </Description>  
         <AboutImage className="about-img">
           <Img src="/img/prof.png" alt="profile-pic" />
@@ -55,10 +55,58 @@ const Description = styled.div`
   width: 60%;
 `
 
+const H3 = styled.h3`
+  display: flex;
+  padding: 20px 0px;
+  color:  #cbd2e6;
+  font-size: 1.5rem;
+  align-items: center;
+`
+
+const AboutSpan = styled.span`
+  display: flex;
+  margin-right: 20px;
+  color: #cc2121;
+  font-size: 1rem;
+`
+
+const AboutMeContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 30px;
+`
+
+const ContentParagraph = styled.p`
+  display: flex;
+  padding-top: 20px;
+  width: 100%;
+  line-height: 1.6;
+  font-size: 0.8rem;
+`
+
+const TechStackContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  padding-top: 30px;
+`
+
+const List = styled.li`
+  &::before{
+    content: "▹";
+    line-height: 12px;
+    margin-right: 20px;
+    color: #cc2121;
+  }
+  list-style: none;
+  display: flex;
+  align-items: center;
+  font-size: 0.8rem;
+  padding-top: 10px;
+`
+
 const AboutImage = styled.div`
   display: flex;
   width: 40%;
-  justify-content: center;
 `
 
 const Img = styled.img`
