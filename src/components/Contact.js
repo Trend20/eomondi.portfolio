1ba9dcee28fed5b47839fc2c8c88a4d7a349cac6
Enock Omondi <enockomondi305@2019>
2022-06-05 22:10:29 +0300

    styling the contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,14 +9,14 @@ const Contact = () =>{
         What's Next?
       </h3>
 
-      <div className="contact-details">
+      <ContactDetails className="contact-details">
         <h1>Get In Touch</h1>
         <p>
           Although I’m not currently looking for any new opportunities, my inbox is always open. Whether you have
           a question or just want to say hi, I’ll try my best to get back to you!
         </p>
         <button>Say Hello</button>
-      </div>
+      </ContactDetails>
     </ContactContainer>
   )
 }
@@ -27,6 +27,58 @@ flex-direction: column;
 width: 100%;
 justify-content: center;
 align-items: center;
+padding: 100px;
+
+h3{
+display: flex;
+padding: 20px 0px;
+color:  #cc2121;
+font-size: 1.5rem;
+align-items: center;
+}
+
+span{
+display: flex;
+margin-right: 20px;
+color: #cc2121;
+font-size: 1rem;
+}
+`
+const ContactDetails = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+
+h1{
+  color: #cbd2e6;
+  font-size: 2.5rem;
+}
+
+p{
+  line-height: 1.5;
+  text-align: center;
+  padding: 20px 0px;
+  width: 70%;
+  font-size: 0.9rem;
+}
+
+button{
+  display: flex;
+  width: 20%;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  background: transparent;
+  outline: none;
+  border: 1px solid #cc2121;
+  border-radius: 5px;
+  padding: 15px;
+  color: #cc2121;
+  font-weight: 500;
+  margin: 50px 0px;
+  cursor: pointer;
+}
 `
 
 export default Contact;
