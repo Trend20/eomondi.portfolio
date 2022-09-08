441f576a579cc87257c19f6ffefab32c94af9c53
Trend20 <enockomondi305@gmail.com>
2022-09-08 12:00:26 +0300

    adding project

--- src/components/Work.js
+++ src/components/Work.js
@@ -106,6 +106,36 @@ const Work = () =>{
         </ProjectDescription>
       </ProjectsWrapper>
 
+      {/* fourth project */}
+
+      <SecondProjectsWrapper>
+      {/* data-aos="fade-up" data-aos-duration="2000" */}
+        <SecondProjectDescription>
+          <h5>Featured Project</h5>
+          <h3>Skyline</h3>
+          <p>
+          Skyline is an agency website that showcases all the services that they offer. 
+          They build templates and showcase them on the Moto Templates website where customers can purchase them.
+          </p>
+          <Stack style={{ justifyContent: 'flex-start' }}>
+            <li>React</li>
+          </Stack>
+          <ProjectLinks style={{ justifyContent: 'flex-start' }}>
+            <a href="https://github.com/Trend20/Skyline" target="_blank">
+              <FiGithub />
+            </a>
+            <a href="https://skylineio.netlify.app/" target="_blank">
+              <FiExternalLink />
+            </a>
+          </ProjectLinks>
+        </SecondProjectDescription>
+        <ProjectImage>
+          <a href="https://skylineio.netlify.app/" target="_blank">
+            <img src="/img/skyline.png" alt="project4" />
+          </a>
+        </ProjectImage>
+      </SecondProjectsWrapper>
+
       <ShowMoreButton>
         <a href="https://github.com/Trend20" target="_blank">Show More</a>
       </ShowMoreButton>
