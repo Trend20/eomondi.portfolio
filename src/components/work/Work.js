2359c76e17abfb3c1e40877bcb6cdd886ec44606
Trend20 <enockomondi305@gmail.com>
2023-10-16 15:00:22 +0300

    work section refactor

--- src/components/work/Work.js
+++ src/components/work/Work.js
@@ -1,35 +1,29 @@
 import React from "react";
-import styled from "styled-components";
 import { FiExternalLink, FiGithub } from "react-icons/fi";
 import { workData } from "../../data/myData";
+import "./Work.css";
 
 const Work = () => {
   return (
-    <WorkContainer className="work" id="work">
+    <div className="work" id="work">
       <div class="head">
         <h1 id="notable">Notable Projects</h1>
         <p>
           <span></span>Some Things I’ve Built
         </p>
       </div>
-      <ProjectsWrapper>
+      <div className="projects_wrapper">
         {workData.map((project) => (
-          <Project>
-            <ProjectImage>
+          <div className="project">
+            <div className="project_image">
               <a href={project.projectLink} target="_blank" rel="noreferrer">
                 <img src={project.img} alt={project.title} />
               </a>
-            </ProjectImage>
-            <ProjectDescription>
+            </div>
+            <div className="project_description">
               <h3>{project.title}</h3>
               <p>{project.description}</p>
-              {/* <Stack>
-                <li>MongoDB</li>
-                <li>ExpressJS</li>
-                <li>React</li>
-                <li>NodeJS</li>
-              </Stack> */}
-              <ProjectLinks>
+              <div className="project_links">
                 <a
                   href={project.projectLink}
                   target="_blank"
@@ -46,177 +40,19 @@ const Work = () => {
                 >
                   Source Code <FiGithub />
                 </a>
-              </ProjectLinks>
-            </ProjectDescription>
-          </Project>
+              </div>
+            </div>
+          </div>
         ))}
-      </ProjectsWrapper>
+      </div>
 
-      <ShowMoreButton>
+      <button className="show_more_button">
         <a href="https://github.com/Trend20" target="_blank" rel="noreferrer">
           Show More
         </a>
-      </ShowMoreButton>
-    </WorkContainer>
+      </button>
+    </div>
   );
 };
-
-const WorkContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  margin: auto;
-  margin-bottom: 50px;
-  color: #8892b0;
-
-  @media screen and (max-width: 768px) {
-    padding: 20px;
-  }
-`;
-
-const ProjectsWrapper = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, 2fr);
-  grid-gap: 40px;
-  justify-content: center;
-  align-items: center;
-  width: 50%;
-  margin-top: 50px;
-
-  @media screen and (max-width: 768px) {
-    display: grid;
-    grid-template-columns: repeat(1, 2fr);
-    width: 100%;
-    grid-gap: 20px;
-  }
-`;
-
-const Project = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-  border: 1px solid #cc2121;
-  border-radius: 10px;
-  padding: 20px;
-
-  @media screen and (max-width: 768px) {
-    padding: 20px;
-  }
-`;
-
-const ProjectImage = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  a {
-    width: 100%;
-  }
-  img {
-    display: flex;
-    max-width: 100%;
-    height: auto;
-    background-color: #cc2121;
-  }
-
-  @media screen and (max-width: 768px) {
-    width: 100%;
-  }
-`;
-const ProjectDescription = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-
-  h3 {
-    color: #cbd2e6;
-    font-size: 1.2rem;
-    margin-top: 20px;
-  }
-
-  p {
-    font-size: 0.8rem;
-    background: #112240;
-    margin-top: 20px;
-    line-height: 1.8;
-    padding: 10px;
-  }
-
-  @media screen and (max-width: 768px) {
-    display: flex;
-    flex-direction: column;
-    width: auto;
-    text-align: left;
-
-    h3 {
-      color: #cbd2e6;
-      font-size: 1.2rem;
-      padding-bottom: 10px;
-    }
-
-    p {
-      font-size: 0.7rem;
-    }
-  }
-`;
-
-const ProjectLinks = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin-top: 20px;
-
-  a {
-    color: #8892b0;
-
-    &:hover {
-      color: #cc2121;
-    }
-  }
-
-  @media screen and (max-width: 768px) {
-    width: 100%;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-  }
-`;
-
-const ShowMoreButton = styled.button`
-  display: flex;
-  width: 10%;
-  justify-content: center;
-  align-items: center;
-  margin: auto;
-  margin-top: 30px;
-  background: transparent;
-  outline: none;
-  border: 1px solid #cc2121;
-  border-radius: 5px;
-
-  a {
-    display: flex;
-    text-decoration: none;
-    width: 100%;
-    justify-content: center;
-    align-items: center;
-    padding: 20px;
-    color: #cc2121;
-    font-weight: 700;
-    font-size: 1rem;
-  }
-
-  &:hover {
-    background: rgb(39, 39, 66);
-  }
-
-  @media screen and (max-width: 768px) {
-    width: 50%;
-  }
-`;
 
 export default Work;
